b3d127944302d1a1e8599174a2461b63f05eade9
Christoph Perger <christoph.perger@spatial-focus.net>
2021-09-07 11:13:49 +0200

    Added appsettings and scaffolding main

--- src/CLCPlusStitcher/Program.cs
+++ src/CLCPlusStitcher/Program.cs
@@ -5,12 +5,50 @@
 namespace CLCPlusStitcher
 {
 	using System;
+	using System.Diagnostics;
+	using System.Threading.Tasks;
+	using ClcPlusRetransformer.Core.Processors;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Logging;
+	using NetTopologySuite.Geometries;
+	using Serilog;
 
 	public class Program
 	{
-		private static void Main(string[] args)
+		public static async Task Main()
 		{
-			Console.WriteLine("Hello World!");
+			IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+			IConfigurationRoot config = builder.Build();
+
+			ServiceCollection serviceCollection = new();
+			serviceCollection.AddLogging(loggingBuilder =>
+				loggingBuilder.AddSerilog(new LoggerConfiguration().ReadFrom.Configuration(config.GetSection("Logging")).CreateLogger()));
+
+			serviceCollection.AddTransient(typeof(Processor<>));
+			serviceCollection.AddTransient(typeof(ChainedProcessor<,>));
+			serviceCollection.AddTransient<ProcessorFactory>();
+
+			ServiceProvider provider = serviceCollection.BuildServiceProvider();
+
+			ILogger<Program> logger = provider.GetRequiredService<ILogger<Program>>();
+
+			logger.LogInformation("Workflow started");
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			PrecisionModel precisionModel = new(int.Parse(config["Precision"]));
+
+			// TODO: Magic!
+
+			stopwatch.Stop();
+			logger.LogInformation("Workflow finished in {Time}ms", stopwatch.ElapsedMilliseconds);
+
+			if (bool.Parse(config["WaitForUserInputAfterCompletion"]))
+			{
+				Console.WriteLine("=== PRESS A KEY TO PROCEED ===");
+				Console.ReadKey();
+			}
 		}
 	}
 }
